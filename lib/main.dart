7a2c0560a3fa582d4d6a335a8c479c659220fc4a
Misganw Meseret <dev.misganaw.meseret@gmail.com>
2022-07-28 10:00:02 +0300

    dissect the hello rectangle

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,30 @@
 import 'package:flutter/material.dart';
 
+class HelloRectangle extends StatelessWidget {
+  @override
+  Widget build(BuildContext contect) {
+    return Center(
+        child: Container(
+            color: Colors.greenAccent,
+            height: 400.0,
+            width: 300.0,
+            child: Center(
+                child: Text(
+              "Hello Rectangle",
+              style: TextStyle(fontSize: 40.0),
+            ))));
+  }
+}
+
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Rectangle',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello Rectangle1'),
+          title: Text("Hello Rectangle"),
         ),
-        body: const HelloRectangle(),
+        body: HelloRectangle(),
       ),
     ),
   );
-}
-
-class HelloRectangle extends StatelessWidget {
-  const HelloRectangle({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-        child: const Center(
-          child: Text(
-            'Hello!!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ),
-    );
-  }
 }
