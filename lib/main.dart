63b8fb82f9d8cc1d26928f99b6affe4270d3cafe
Misganw Meseret <dev.misganaw.meseret@gmail.com>
2022-07-30 09:50:32 +0300

    category widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
 
-class HelloRectangle extends StatelessWidget {
-  @override
-  Widget build(BuildContext contect) {
-    return Center(
-        child: Container(
-            color: Colors.greenAccent,
-            height: 400.0,
-            width: 300.0,
-            child: Center(
-                child: Text(
-              "Hello Rectangle",
-              style: TextStyle(fontSize: 40.0),
-            ))));
-  }
+// You can use a relative import, i.e. `import 'category.dart';` or
+// a package import, as shown below.
+// More details at http://dart-lang.github.io/linter/lints/avoid_relative_lib_imports.html
+import 'category.dart';
+
+// TODO: Pass this information into your custom [Category] widget
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
+
+/// The function that is called when main.dart is run.
+void main() {
+  runApp(const UnitConverterApp());
 }
 
-void main() {
-  runApp(
-    MaterialApp(
+/// This widget is the root of our application.
+/// Currently, we just show one widget in our app.
+class UnitConverterApp extends StatelessWidget {
+  const UnitConverterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Rectangle"),
+        backgroundColor: Colors.green[100],
+        body: const Center(
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
+          ),
         ),
-        body: HelloRectangle(),
       ),
-    ),
-  );
+    );
+  }
 }
